9d95b4ff65fd414508e5329e8f11ee7f745910d3
Vladimir-Serebrennikov <serebvl23@gmail.com>
2024-01-03 20:46:29 +0300

    update build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,24 +24,20 @@ application {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.assertj:assertj-core:3.23.1")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.assertj:assertj-core:3.24.2")
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("com.squareup.okhttp3:mockwebserver:4.11.0")
     implementation("io.javalin:javalin:5.6.2")
     implementation("io.javalin:javalin-bundle:5.6.2")
     implementation("org.slf4j:slf4j-simple:2.0.7")
-    implementation("io.javalin:javalin-rendering:5.6.0")
+    implementation("io.javalin:javalin-rendering:5.6.2")
     implementation("gg.jte:jte:3.0.1")
     implementation("com.h2database:h2:2.2.220")
     implementation("com.zaxxer:HikariCP:5.0.1")
     implementation("com.konghq:unirest-java:3.14.5")
     implementation("org.jsoup:jsoup:1.16.2")
     implementation("org.postgresql:postgresql:42.6.0")
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
 
 tasks.test {
