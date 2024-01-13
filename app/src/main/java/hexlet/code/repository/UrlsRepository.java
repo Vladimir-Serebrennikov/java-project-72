486064a91caaca5b0eb4036440f59514eeb92093
Vladimir-Serebrennikov <135636572+Vladimir-Serebrennikov@users.noreply.github.com>
2024-01-13 14:26:32 +0300

    Update UrlsRepository.java

--- app/src/main/java/hexlet/code/repository/UrlsRepository.java
+++ app/src/main/java/hexlet/code/repository/UrlsRepository.java
@@ -1,6 +1,5 @@
 package hexlet.code.repository;
 
-import java.sql.Timestamp;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Optional;
@@ -15,7 +14,6 @@ public class UrlsRepository extends BaseRepository {
         String sql = "INSERT INTO urls (name, created_at) VALUES (?, ?)";
         try (var conn = dataSource.getConnection();
              var preparedStatement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-            url.setCreatedAt(new Timestamp(System.currentTimeMillis()));
             preparedStatement.setString(1, url.getName());
             preparedStatement.setTimestamp(2, url.getCreatedAt());
             preparedStatement.executeUpdate();
